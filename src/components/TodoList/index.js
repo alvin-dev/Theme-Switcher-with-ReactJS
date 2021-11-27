177b0782b5c09b02fef82a44f0e527bda86be799
Alvaro <alvinweeb@gmail.com>
2021-11-27 16:36:00 -0300

    first commit

---
+++ src/components/TodoList/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './style.css'
+
+const TodoList = () => (
+  <ul>
+    <li>Praticar inglês</li>
+    <li>Estudar e pra ticar programação</li>
+    <li>Fazer commit no GitHub</li>
+  </ul>
+);
+
+export default TodoList;
